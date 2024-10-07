21ef17329fec8fb0e2d370452d152bcffacc5931
Decka1705 <103016311+Decka1705@users.noreply.github.com>
2024-10-07 15:59:10 +0700

    Add files via upload

---
+++ BlockingServer.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.*;
+
+public class BlockingServer {
+    private static final int PORT = 1234;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT);
+             Socket clientSocket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            System.out.println("Server is listening on port " + PORT);
+
+            String message = in.readLine();
+            System.out.println("Received message from client: " + message);
+
+            out.println("Halo Guys");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
